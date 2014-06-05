ddcf33af838fbc08d8a181c954645651b5adb130
Andrea Reginato <andrea.reginato@gmail.com>
2014-06-05 15:44:54 +0200

    Added tests to cover new Profile definition

--- test/spec/services/profile.js
+++ test/spec/services/profile.js
@@ -2,7 +2,8 @@
 
 describe('Profile', function() {
 
-  var $rootScope, $location, $httpBackend, $http, AccessToken, result, date, callback;
+  var $rootScope, $location, $httpBackend, $http, AccessToken, Profile;
+  var result, date, callback;
 
   var fragment = 'access_token=token&token_type=bearer&expires_in=7200&state=/path';
   var headers  = { 'Accept': 'application/json, text/plain, */*', 'Authorization': 'Bearer token' }
@@ -16,6 +17,7 @@ describe('Profile', function() {
   beforeEach(inject(function($injector) { $httpBackend    = $injector.get('$httpBackend') }));
   beforeEach(inject(function($injector) { $http           = $injector.get('$http') }));
   beforeEach(inject(function($injector) { AccessToken     = $injector.get('AccessToken') }));
+  beforeEach(inject(function($injector) { Profile         = $injector.get('Profile') }));
 
   beforeEach(function() { callback = jasmine.createSpy('callback') });
 
@@ -33,52 +35,70 @@ describe('Profile', function() {
         $httpBackend.whenGET('http://example.com/me', headers).respond(resource);
       });
 
-      it('makes the request', inject(function(Profile) {
-        $httpBackend.expect('GET', 'http://example.com/me');
-        Profile.get(params.profileUri);
-        $rootScope.$apply();
-        $httpBackend.flush();
-      }));
+      describe('when gets the profile', function() {
 
-      it('gets the resource', inject(function(Profile) {
-        Profile.get(params.profileUri).success(function(response) { result = response });
-        $rootScope.$apply();
-        $httpBackend.flush();
-        expect(result.name).toEqual('Alice');
-      }));
-
-
-      describe('when expired', function() {
-
-        beforeEach(function() {
-          $rootScope.$on('oauth:expired', callback);
-        });
-
-        beforeEach(function() {
-          date = new Date();
-          date.setTime(date.getTime() + 86400000);
-        });
-
-        beforeEach(function() {
-          Timecop.install();
-          Timecop.travel(date); // go one day in the future
-        });
-
-        beforeEach(function() {
-        })
-
-        afterEach(function() {
-          Timecop.uninstall();
-        });
-
-        it('fires the oauth:expired event', inject(function(Profile) {
-          Profile.get(params.profileUri);
+        it('makes the request', function() {
+          $httpBackend.expect('GET', 'http://example.com/me');
+          Profile.find(params.profileUri);
           $rootScope.$apply();
           $httpBackend.flush();
-          var event = jasmine.any(Object);
-          var token = jasmine.any(Object);
-          expect(callback).toHaveBeenCalledWith(event, token);
+        });
+
+        it('gets the resource', inject(function(Profile) {
+          Profile.find(params.profileUri).success(function(response) { result = response });
+          $rootScope.$apply();
+          $httpBackend.flush();
+          expect(result.name).toEqual('Alice');
         }));
+
+        it('caches the profile', function() {
+          Profile.find(params.profileUri);
+          $rootScope.$apply();
+          $httpBackend.flush();
+          expect(Profile.get().name).toEqual('Alice');
+        });
+
+        describe('when expired', function() {
+
+          beforeEach(function() {
+            $rootScope.$on('oauth:expired', callback);
+          });
+
+          beforeEach(function() {
+            date = new Date();
+            date.setTime(date.getTime() + 86400000);
+          });
+
+          beforeEach(function() {
+            Timecop.install();
+            Timecop.travel(date); // go one day in the future
+          });
+
+          afterEach(function() {
+            Timecop.uninstall();
+          });
+
+          it('fires the oauth:expired event', inject(function(Profile) {
+            Profile.find(params.profileUri);
+            $rootScope.$apply();
+            $httpBackend.flush();
+            var event = jasmine.any(Object);
+            var token = jasmine.any(Object);
+            expect(callback).toHaveBeenCalledWith(event, token);
+          }));
+        });
+      });
+
+
+      describe('when sets the profile', function() {
+
+        beforeEach(function() {
+          Profile.set(resource);
+        });
+
+        it('caches the profile', function() {
+          expect(Profile.get().name).toEqual('Alice');
+        });
       });
     });
   });
