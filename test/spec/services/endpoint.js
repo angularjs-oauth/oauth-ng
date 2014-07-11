aaa9c6ac5d338dccae6b1a3259387956bbbe0d95
ryanp <ryanp@doapps.com>
2014-07-10 21:47:39 -0500

    modify and add test cases for state changes

--- test/spec/services/endpoint.js
+++ test/spec/services/endpoint.js
@@ -6,14 +6,13 @@ describe('Endpoint', function() {
 
   var fragment = 'access_token=token&token_type=bearer&expires_in=7200&state=/path';
   var params   = { site: 'http://example.com', clientId: 'client-id', redirectUri: 'http://example.com/redirect', scope: 'scope', authorizePath: '/oauth/authorize' };
-  var uri      = 'http://example.com/oauth/authorize?response_type=token&client_id=client-id&redirect_uri=http://example.com/redirect&scope=scope&state=/';
+  var uri      = 'http://example.com/oauth/authorize?response_type=token&client_id=client-id&redirect_uri=http%3A%2F%2Fexample.com%2Fredirect&scope=scope&state=';
 
   beforeEach(module('oauth'));
 
   beforeEach(inject(function($injector) { $location       = $injector.get('$location') }));
   beforeEach(inject(function($injector) { $sessionStorage = $injector.get('$sessionStorage') }));
   beforeEach(inject(function($injector) { Endpoint        = $injector.get('Endpoint') }));
-
 
   describe('#set', function() {
 
@@ -35,12 +34,44 @@ describe('Endpoint', function() {
         result = Endpoint.set(params);
       });
 
-      it('returns previous path in status', function() {
-        expect(result).toEqual(uri + 'path');
+      it('uri should not be in state', function() {
+        expect(result).toEqual(uri);
+      });
+    });
+
+    describe('set state', function() {
+      var paramsClone = JSON.parse(JSON.stringify(params));
+
+      beforeEach(function() {
+          paramsClone.state = 'test';
+      });
+
+      beforeEach(function() {
+          result = Endpoint.set(paramsClone);
+      });
+
+      it('uri should not be in state', function() {
+          expect(result).toEqual(uri + 'test');
+      });
+    });
+
+    describe('authorizePath can have query string it in', function() {
+      var paramsClone = JSON.parse(JSON.stringify(params));
+
+      beforeEach(function() {
+          paramsClone.authorizePath = '/oauth/authorize?google=doesthis';
+      });
+
+      beforeEach(function() {
+          result = Endpoint.set(paramsClone);
+      });
+
+      it('uri should not be in state', function() {
+          var expectedUri = 'http://example.com/oauth/authorize?google=doesthis&response_type=token&client_id=client-id&redirect_uri=http%3A%2F%2Fexample.com%2Fredirect&scope=scope&state=';
+          expect(result).toEqual(expectedUri);
       });
     });
   });
-
 
   describe('#get', function() {
 
