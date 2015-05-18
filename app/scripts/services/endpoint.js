3dd29d5b7a1b6a7b690bbb754678da15411e89ed
Mark Eschbach <meschbach@skyslope.com>
2015-05-18 09:29:18 -0700

    Calculate path in Endpoint#get/1

--- app/scripts/services/endpoint.js
+++ app/scripts/services/endpoint.js
@@ -5,45 +5,44 @@ var endpointClient = angular.module('oauth.endpoint', []);
 endpointClient.factory('Endpoint', function() {
 
   var service = {};
-  var url;
-
 
   /*
    * Defines the authorization URL
    */
 
-  service.set = function(params) {
-    var oAuthScope = (params.scope) ? params.scope : '',
-        state = (params.state) ? encodeURIComponent(params.state) : '',
-        authPathHasQuery = (params.authorizePath.indexOf('?') == -1) ? false : true,
-        appendChar = (authPathHasQuery) ? '&' : '?';    //if authorizePath has ? already append OAuth2 params
-
-    url = params.site +
-          params.authorizePath +
-          appendChar + 'response_type=' + params.responseType + '&' +
-          'client_id=' + encodeURIComponent(params.clientId) + '&' +
-          'redirect_uri=' + encodeURIComponent(params.redirectUri) + '&' +
-          'scope=' + oAuthScope + '&' +
-          'state=' + state;
-
-    return url;
+  service.set = function(configuration) {
+    this.config = configuration;
+    return this.get();
   };
 
   /*
    * Returns the authorization URL
    */
 
-  service.get = function() {
+  service.get = function( overrides ) {
+    var params = angular.extend( {}, service.config, overrides);
+    var oAuthScope = (params.scope) ? params.scope : '',
+        state = (params.state) ? encodeURIComponent(params.state) : '',
+        authPathHasQuery = (params.authorizePath.indexOf('?') == -1) ? false : true,
+        appendChar = (authPathHasQuery) ? '&' : '?';    //if authorizePath has ? already append OAuth2 params
+
+    var url = params.site +
+          params.authorizePath +
+          appendChar + 'response_type=' + params.responseType + '&' +
+          'client_id=' + encodeURIComponent(params.clientId) + '&' +
+          'redirect_uri=' + encodeURIComponent(params.redirectUri) + '&' +
+          'scope=' + oAuthScope + '&' +
+          'state=' + state;
     return url;
   };
-
 
   /*
    * Redirects the app to the authorization URL
    */
 
   service.redirect = function() {
-    window.location.replace(url);
+    var targetLocation = this.get();
+    window.location.replace(targetLocation);
   };
 
   return service;
--- test/spec/services/endpoint.js
+++ test/spec/services/endpoint.js
@@ -96,12 +96,75 @@ describe('Endpoint', function() {
       Endpoint.set(params);
     });
 
-    beforeEach(function() {
-      result = Endpoint.get();
+    describe( "without overrides", function(){
+      beforeEach(function() {
+        result = Endpoint.get();
+      });
+
+      it('returns the oauth server endpoint', function() {
+        expect(result).toEqual(uri);
+      });
     });
 
-    it('returns the oauth server endpoint', function() {
-      expect(result).toEqual(uri);
+    describe( "with state override", function(){
+      it( "injects the state correct", function(){
+        var override = { state: 'testState' };
+        var result = Endpoint.get( override );
+        expect( result ).toEqual( uri + encodeURIComponent( override.state ) );
+      });
+    });
+
+    describe( "with clientId override", function(){
+      it( "injects the override", function(){
+        var override    = { clientId: 'unicorn' };
+        var result      = Endpoint.get( override );
+        var expectedUri = 'http://example.com/oauth/authorize?response_type=token&client_id=unicorn&redirect_uri=http%3A%2F%2Fexample.com%2Fredirect&scope=scope&state=';
+        expect( result ).toEqual( expectedUri );
+      });
+    });
+
+    describe( "with scope override", function(){
+      it( "injects the override", function(){
+        var override    = { scope: 'stars' };
+        var result      = Endpoint.get( override );
+        var expectedUri = 'http://example.com/oauth/authorize?response_type=token&client_id=client-id&redirect_uri=http%3A%2F%2Fexample.com%2Fredirect&scope=stars&state=';
+        expect( result ).toEqual( expectedUri );
+      });
+    });
+
+    describe( "with state override", function(){
+      it( "injects the state correct", function(){
+        var override = { state: 'testState' };
+        var result   = Endpoint.get( override );
+        expect( result ).toEqual( uri + encodeURIComponent( override.state ) );
+      });
+    });
+
+    describe( "with responseType override", function(){
+      it( "injects the correct repsonseType", function(){
+        var override    = { responseType: 'id_token' };
+        var result      = Endpoint.get( override );
+        var expectedUri = 'http://example.com/oauth/authorize?response_type=id_token&client_id=client-id&redirect_uri=http%3A%2F%2Fexample.com%2Fredirect&scope=scope&state=';
+        expect( result ).toEqual( expectedUri );
+      });
+    });
+
+    describe( "with site override", function(){
+      it( "injects the correct site", function(){
+        var override    = { site: 'https://invincible.test' };
+        var result      = Endpoint.get( override );
+        var expectedUri = 'https://invincible.test/oauth/authorize?response_type=token&client_id=client-id&redirect_uri=http%3A%2F%2Fexample.com%2Fredirect&scope=scope&state=';
+        expect( result ).toEqual( expectedUri );
+      });
+    });
+
+    describe( "with authorize path overrides", function(){
+      it( "injects the correct authorize path", function(){
+        var override    = { authorizePath: '/end/here' };
+        var result      = Endpoint.get( override );
+        var expectedUri = 'http://example.com/end/here?response_type=token&client_id=client-id&redirect_uri=http%3A%2F%2Fexample.com%2Fredirect&scope=scope&state=';
+        expect( result ).toEqual( expectedUri );
+      });
     });
   });
 });
