35bf57d1c4e03a744c4ebf6861643cb8c8d69faa
Alfsig <richaudeau.thomas@gmail.com>
2016-12-12 16:39:13 +0100

    Update oauth-ng.js

--- dist/oauth-ng.js
+++ dist/oauth-ng.js
@@ -684,8 +684,8 @@ profileClient.factory('Profile', ['$http', 'AccessToken', '$rootScope', function
 
   service.find = function(uri) {
     var promise = $http.get(uri, { headers: headers() });
-    promise.success(function(response) {
-        profile = response;
+    promise.then(function(response) {
+        profile = response.data;
         $rootScope.$broadcast('oauth:profile', profile);
       });
     return promise;
@@ -898,8 +898,8 @@ directives.directive('oauth', [
       };
 
       var compile = function() {
-        $http.get(scope.template, { cache: $templateCache }).success(function(html) {
-          element.html(html);
+        $http.get(scope.template, { cache: $templateCache }).then(function(html) {
+          element.html(html.data);
           $compile(element.contents())(scope);
         });
       };
@@ -908,8 +908,8 @@ directives.directive('oauth', [
         var token = AccessToken.get();
 
         if (token && token.access_token && scope.profileUri) {
-          Profile.find(scope.profileUri).success(function(response) {
-            scope.profile = response;
+          Profile.find(scope.profileUri).then(function(response) {
+            scope.profile = response.data;
           });
         }
       };
